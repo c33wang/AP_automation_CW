aca3c5e68a4f2889bf8f8c2736a7dc2ee7b72905
ubnt <ubntchun@gmail.com>
2015-12-11 16:16:27 -0800

    2G channel loop

--- runtest.py
+++ runtest.py
@@ -7,15 +7,17 @@ import time
 
 
 #Screen Room Settling
-iperf3_TX = "iperf3 -f m -t 300 -O 3 -c 192.168.1.174 -P 10"
-iperf3_RX = "iperf3 -f m -t 300 -O 3 -c 192.168.1.174 -P 10 -R"
-AP_iPaddress = "192.168.1.227"
+#iperf3_TX = "iperf3 -f m -t 300 -O 3 -c 192.168.1.174 -P 10"
+#iperf3_RX = "iperf3 -f m -t 300 -O 3 -c 192.168.1.174 -P 10 -R"
+#AP_iPaddress = "192.168.1.227"
 
 
 iperf3_TX = "iperf3 -f m -t 6 -O 3 -c 192.168.1.236 -P 10"
 iperf3_RX = "iperf3 -f m -t 6 -O 3 -c 192.168.1.236 -P 10 -R"
 AP_iPaddress = "192.168.2.24"
 
+
+##############################################----5G----###############################################################
 def max_throughput_5_ch(AP_iPaddress):
     for x in [20, 40, 80]:
         print "5G VHT%d TX:" %x
@@ -86,6 +88,70 @@ def chainmask_throughput(mIPaddress):
 
 
 
+##############################################----2G----###############################################################
+
+
+def max_throughput_2_ch(AP_iPaddress):
+    for x in [20, 40]:
+        print "2G VHT%d TX:" %x
+        controllerAP = AccessPoint(AP_iPaddress)
+        controllerAP.configure_2g_channel_width(x)
+        controllerAP.quit_browser()
+        time.sleep(10)
+        for ch in [1,2,3,4,5,6,7,8,9,10,11]:
+            controllerAP = AccessPoint(AP_iPaddress)
+            controllerAP.configure_2g_channel(ch)
+            controllerAP.quit_browser()
+            print "Channel:" + str(ch)
+            time.sleep(180)
+            out = subprocess.check_output(iperf3_TX, shell=True)
+            outputlist = out.split('\n')
+            print outputlist[len(outputlist)-5]
+            print outputlist[len(outputlist)-4]
+
+            print "2G VHT%d RX:" %x
+            out = subprocess.check_output(iperf3_RX, shell=True)
+            outputlist = out.split('\n')
+            print outputlist[len(outputlist)-5]
+            print outputlist[len(outputlist)-4]
+
+
+def max_throughput_2(AP_iPaddress):
+    for x in [20, 40]:
+        print "2G VHT%d TX:" %x
+        controllerAP = AccessPoint(AP_iPaddress)
+        controllerAP.configure_2g_channel_width(x)
+        controllerAP.quit_browser()
+        time.sleep(120)
+        out = subprocess.check_output(iperf3_TX, shell=True)
+        outputlist = out.split('\n')
+        print outputlist[len(outputlist)-5]
+        print outputlist[len(outputlist)-4]
+
+        print "2G VHT%d RX:" %x
+        out = subprocess.check_output(iperf3_RX, shell=True)
+        outputlist = out.split('\n')
+        print outputlist[len(outputlist)-5]
+        print outputlist[len(outputlist)-4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def max_throughput_2(mIPaddress):
 
@@ -108,18 +174,6 @@ def max_throughput_2(mIPaddress):
             print outputlist[len(outputlist)-5]
             print outputlist[len(outputlist)-4]
 
-
-
-
-    # sshap.set_chainmask_5(1)
-    # sshap.exec_command('iwpriv wifi1 get_txchainmask')
-    #
-    # sshap.set_chainmask_5(2)
-    # sshap.exec_command('iwpriv wifi1 get_txchainmask')
-    #
-    # sshap.set_chainmask_5(4)
-    # sshap.exec_command('iwpriv wifi1 get_txchainmask')
-    # max_throughput(mIPaddress)
 
 
 
